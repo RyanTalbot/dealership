bd57db134e04eb4f22f6e8b1b97b56ed3a063c97
Ryan Talbot <ryan-talbot@outlook.com>
2021-12-09 22:18:12 +0000

    basic server setup pre run implementation

---
+++ src/Server.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private static final int PORT = 8080;
+
+    private static ArrayList<ClientHandler> clients = new ArrayList<>();
+    private static ExecutorService pool = Executors.newFixedThreadPool(4);
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket listener = new ServerSocket(PORT);
+
+        System.out.println("[SERVER] Waiting for connection...");
+
+        while (true) {
+            Socket client = listener.accept();
+            System.out.println("[SERVER] Connected to client");
+            ClientHandler clientThread = new ClientHandler(client);
+            clients.add(clientThread);
+
+            pool.execute(clientThread);
+        }
+    }
+}
+
+class ClientHandler implements Runnable {
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+
+    public ClientHandler(Socket clientSocket) throws IOException {
+        this.client = clientSocket;
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        out = new PrintWriter(client.getOutputStream(), true);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
