22779e5dc7155568cb88c2fb636750d41602b80e
Ryan Talbot <ryan-talbot@outlook.com>
2021-12-09 23:00:03 +0000

    car class core

--- src/Server.java
+++ src/Server.java
@@ -48,13 +48,13 @@ class ClientHandler implements Runnable {
                 String request = in.readLine();
 
                 if (request.contains("add")) {
-                    System.out.println("ADD");
+                    out.println("ADD");
                     add();
                 } else if (request.contains("sell")) {
-                    System.out.println("SELL");
+                    out.println("SELL");
                     sell();
                 } else if (request.contains("info")) {
-                    System.out.println("INFO");
+                    out.println("INFO");
                     carInfo();
                 }
             }
@@ -80,5 +80,59 @@ class ClientHandler implements Runnable {
 
     public void carInfo() {
 
+    }
+}
+
+class Car {
+    private String registration, make;
+    private int price, mileage;
+    private boolean forSale;
+
+    public Car(String registration, String make, int price, int mileage, boolean forSale) {
+        this.registration = registration;
+        this.make = make;
+        this.price = price;
+        this.mileage = mileage;
+        this.forSale = forSale;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public boolean isForSale() {
+        return forSale;
+    }
+
+    public void setForSale(boolean forSale) {
+        this.forSale = forSale;
     }
 }
